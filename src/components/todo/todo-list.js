2b1437aed61f881ba25712748c7dc7806c75e32b
Cem Ozturan <cemozturan@gmail.com>
2017-02-01 16:00:10 +0000

    pulling todo list item into its own stateless component

--- src/components/todo/todo-list.js
+++ src/components/todo/todo-list.js
@@ -1,12 +1,10 @@
 import React from 'react';
+import {TodoItem} from './todo-item';
 
 export const TodoList = (props) => (
   <div className="Todo-List">
     <ul>
-      {props.todos.map(todo =>
-        <li key={todo.id}>
-          <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
-        </li>)}
+      {props.todos.map(todo => <TodoItem key={todo.id} {...todo}/>)}
     </ul>
   </div>
 );
