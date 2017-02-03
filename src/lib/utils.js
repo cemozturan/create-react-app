8e26612ea4bc079752aafb8204a085cdaa1161ac
Cem Ozturan <cemozturan@gmail.com>
2017-02-03 17:04:40 +0000

    adding utils to pipe functions which allow function composition

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,7 @@
 export const partial = (fn, ...args) => { // ...args here uses the rest operator
   return fn.bind(null, ...args); // ...args here uses the spread operator, different than the one above!
 }
+
+const _pipe = (f, g) => (...args) => g(f(...args));
+
+export const pipe = (...fns) => fns.reduce(_pipe);
